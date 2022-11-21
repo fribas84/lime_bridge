038f7042593f1a0bf14ba60ce84c317090037d64
fribas <fribas@asapp.com>
2022-11-21 18:14:53 -0300

    requestTransaction

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -7,14 +7,37 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 contract Bridge is AccessControl{
 
+    uint public constant LOCK_TIME = 45 seconds;
+    enum Network{GOERLI,MUMBAI,BSC}
     address private _LMT;
 
     struct txToBridge(
         address orginator;
         uint amount;
+        uint timeLock;
+        Network network;
+        bool withdrawn;
+        bool refunded;
         bool isDone;
+        bool exists; 
     )
 
+    mapping(bytes32=>txToBridge) TransferIDMapping;
+
+    event TokenAddressChanged(address user,address newTokenAddress);
+    event NewTransferBridgeRequest(address user,uint amount,Network network )
+
+    modifier tokensTransferable(address _sender, uint256 _amount) {
+        require(_amount > 0, "LMT amount must be > 0");
+        require(IERC20(_LMT).allowance(_sender, address(this)) >= _amount,
+            "LMT allowance must be >= amount");
+        _;
+    }
+
+    modifier futureTimelock(uint256 _time) {
+        require(_time > now, "Timelock time must be in the future");
+        _;
+    }
     constructor(address lmtAddress){
         _grantRole(DEFAULT_ADMIN_ROLE,msg.sender);
         _setLMT(lmtAddress);
@@ -33,6 +56,53 @@ contract Bridge is AccessControl{
         emit TokenAddressChanged(user,_LMT);
     }
 
-    event TokenAddressChanged(address user,address newTokenAddress);
-    event NewBridgeCross()
+
+    function requestTransaction(
+        uint _amount,
+        Network _destination,
+        bytes32 _hashLock,
+        )
+    public
+    payable
+    tokensTransferable(msg.sender, _amount)
+    returns(bytes32) {
+        return _requestTransaction(
+            msg.sender,
+            _amount,
+            _destination,
+            _hashLock,
+            timeLock)
+
+    }
+    function _requestTransaction(address sender,
+        uint _amount,
+        Network _destination,
+        bytes32 _hashLock)
+        private
+        returns (bytes32) {    
+        uint timeLock = block.timestamp + LOCK_TIME;
+        txToBridge newTxToBridge = (sender,_amount,timeLock,network,false,true);
+        bytes32 transferId = keccak256(abi.encodePacked(
+            sender,
+            _amount,
+            _destination,
+            _hashLock,
+            timeLock,
+        ))
+
+        if(TransferIDMapping[transferId].exists == true){
+            revert("Transfer already in progress");
+        }
+
+        if(!IERC20(_LMT).transferFrom(sender,address(this),_amount)){
+            revert("Transfer from LMT to bridge failed")
+        };
+        TransferIDMapping[transferId] = newTxToBridge;
+        emit NewTransferBridgeRequest(sender,amount,destination);
+        return transferId;
+    }
+
+
+
+
 }
