582906dd8c174496b4c776f89c35c8088f890e1a
fribas <fribas@asapp.com>
2022-11-29 13:44:37 -0300

    doc format

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -7,14 +7,16 @@ import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "hardhat/console.sol";
 
-
-contract Bridge is AccessControl,Pausable, ReentrancyGuard{
-
+contract Bridge is AccessControl, Pausable, ReentrancyGuard {
     uint8 public constant LOCK_TIME = 15 seconds;
-    enum Network{GOERLI,MUMBAI,BSC}
+    enum Network {
+        GOERLI,
+        MUMBAI,
+        BSC
+    }
     address private _LMT;
     uint balaceLMT;
-        
+
     struct txToBridge {
         address orginator;
         uint amount;
@@ -23,21 +25,27 @@ contract Bridge is AccessControl,Pausable, ReentrancyGuard{
         bool withdrawn;
         bool refunded;
         bool isDone;
-        bool exists; 
+        bool exists;
     }
 
-    mapping(address=>uint) withdrawableMapping;
+    mapping(address => uint) withdrawableMapping;
     uint debt;
     Network immutable myNetwork;
-    
 
-    mapping(bytes32=>txToBridge) TransferIDMapping;
-    mapping(bytes32=>txToBridge) InboundTxMapping;
-   
-    mapping(Network=>address) bridgesAddresses;
+    mapping(bytes32 => txToBridge) TransferIDMapping;
+    mapping(bytes32 => txToBridge) InboundTxMapping;
 
-    event TokenAddressChanged(address user,address newTokenAddress);
-    event NewTransferBridgeRequest(address user,uint amount,Network network,uint timelock, bytes32 hashlock, bytes32 id);
+    mapping(Network => address) bridgesAddresses;
+
+    event TokenAddressChanged(address user, address newTokenAddress);
+    event NewTransferBridgeRequest(
+        address user,
+        uint amount,
+        Network network,
+        uint timelock,
+        bytes32 hashlock,
+        bytes32 id
+    );
     event NewTransferAvailable(address user);
     event DestinationTransferCompleted(address user, uint amount);
     event Widthdraw(address user, uint amount);
@@ -50,85 +58,100 @@ contract Bridge is AccessControl,Pausable, ReentrancyGuard{
     // }
     // For further use
     // mapping(address=>bool) validatorsMapping;
-    
+
     modifier tokensTransferable(address _sender, uint256 _amount) {
         require(_amount > 0, "[Tokens Transfer] LMT amount must be > 0");
-        require(IERC20(_LMT).allowance(_sender, address(this)) >= _amount,
-            "[Tokens Transfer] LMT allowance must be >= amount");
+        require(
+            IERC20(_LMT).allowance(_sender, address(this)) >= _amount,
+            "[Tokens Transfer] LMT allowance must be >= amount"
+        );
         _;
     }
 
     modifier futureTimelock(uint256 _time) {
-        require(_time > block.timestamp, "[Timelock] Timelock time must be in the future");
+        require(
+            _time > block.timestamp,
+            "[Timelock] Timelock time must be in the future"
+        );
         _;
     }
 
-    
-    constructor(address lmtAddress, Network _myNetwork){
-        _grantRole(DEFAULT_ADMIN_ROLE,msg.sender);
+    constructor(address lmtAddress, Network _myNetwork) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _setLMT(lmtAddress, msg.sender);
         myNetwork = _myNetwork;
         bridgesAddresses[myNetwork] = address(this);
     }
 
-    function getLMT() public view returns(address){
+    function getLMT() public view returns (address) {
         return _LMT;
     }
 
-    function setLMT(address lmtAddress) public whenNotPaused onlyRole(DEFAULT_ADMIN_ROLE){
-        _setLMT(lmtAddress,msg.sender);
+    function setLMT(
+        address lmtAddress
+    ) public whenNotPaused onlyRole(DEFAULT_ADMIN_ROLE) {
+        _setLMT(lmtAddress, msg.sender);
     }
 
-    function _setLMT(address lmtAddress,address user) internal {
+    function _setLMT(address lmtAddress, address user) internal {
         _LMT = lmtAddress;
-        emit TokenAddressChanged(user,_LMT);
+        emit TokenAddressChanged(user, _LMT);
     }
-
 
     function requestTransaction(
         uint _amount,
         Network _destination,
         bytes32 _hashLock
-        )
-    public
-    payable
-    whenNotPaused
-    tokensTransferable(msg.sender, _amount)
-    returns(bytes32) {
-        return _requestTransaction(
-            msg.sender,
-            _amount,
-            _destination,
-            _hashLock);
-
+    )
+        public
+        payable
+        whenNotPaused
+        tokensTransferable(msg.sender, _amount)
+        returns (bytes32)
+    {
+        return
+            _requestTransaction(msg.sender, _amount, _destination, _hashLock);
     }
-    function _requestTransaction(address sender,
+
+    function _requestTransaction(
+        address sender,
         uint _amount,
         Network _destination,
-        bytes32 _hashLock)
-        private
-        returns (bytes32) {    
+        bytes32 _hashLock
+    ) private returns (bytes32) {
         uint timeLock = block.timestamp + LOCK_TIME;
-        txToBridge memory newTxToBridge = txToBridge(sender,_amount,timeLock,_destination,false,false,false,true);
-        bytes32 transferId = keccak256(abi.encodePacked(
+        txToBridge memory newTxToBridge = txToBridge(
             sender,
             _amount,
+            timeLock,
             _destination,
-            _hashLock
-        ));
+            false,
+            false,
+            false,
+            true
+        );
+        bytes32 transferId = keccak256(
+            abi.encodePacked(sender, _amount, _destination, _hashLock)
+        );
 
-        
-        if(TransferIDMapping[transferId].exists == true){
+        if (TransferIDMapping[transferId].exists == true) {
             revert("[Bridge] Transfer already in progress");
         }
 
-        if(!IERC20(_LMT).transferFrom(sender,address(this),_amount)){
+        if (!IERC20(_LMT).transferFrom(sender, address(this), _amount)) {
             revert("[Bridge] Transfer from LMT to bridge failed");
         }
         TransferIDMapping[transferId] = newTxToBridge;
         // To be used if an external validator is developed.
         //validatorsMapping[sender] = true;
-        emit NewTransferBridgeRequest(sender,_amount,_destination,timeLock,_hashLock,transferId);
+        emit NewTransferBridgeRequest(
+            sender,
+            _amount,
+            _destination,
+            timeLock,
+            _hashLock,
+            transferId
+        );
         return transferId;
     }
 
@@ -136,81 +159,105 @@ contract Bridge is AccessControl,Pausable, ReentrancyGuard{
         uint _amount,
         uint timeLock,
         Network _destination,
-        bytes32 _hashLock, 
-        bytes32 _transferId       
-        )
-        external payable
-        //onlyValidator(msg.sender) 
-        {   
-            uint balance = IERC20(_LMT).balanceOf(address(this)) - debt;
-            require(_amount <= balance,"[Bridge] Not enough balance in bridge ");
-            require(bridgesAddresses[_destination] == address(this), "[Bridge] Wrong bridge destination");
-            _initDestinationTransfer(
-                        msg.sender,
-                        _amount,
-                        timeLock,
-                        _destination,
-                        _hashLock, 
-                        _transferId  
-            );
-
-        }
+        bytes32 _hashLock,
+        bytes32 _transferId
+    ) external payable //onlyValidator(msg.sender)
+    {
+        uint balance = IERC20(_LMT).balanceOf(address(this)) - debt;
+        require(_amount <= balance, "[Bridge] Not enough balance in bridge ");
+        require(
+            bridgesAddresses[_destination] == address(this),
+            "[Bridge] Wrong bridge destination"
+        );
+        _initDestinationTransfer(
+            msg.sender,
+            _amount,
+            timeLock,
+            _destination,
+            _hashLock,
+            _transferId
+        );
+    }
 
     function _initDestinationTransfer(
         address _sender,
         uint _amount,
         uint timeLock,
         Network _destination,
-        bytes32 _hashLock, 
-        bytes32 _transferId  
-
-     ) internal {
-
-        txToBridge memory inboundTxToBridge = txToBridge(_sender,_amount,timeLock,_destination,false,false,false,true);
-        bytes32 transferId = keccak256(abi.encodePacked(
+        bytes32 _hashLock,
+        bytes32 _transferId
+    ) internal {
+        txToBridge memory inboundTxToBridge = txToBridge(
             _sender,
             _amount,
+            timeLock,
             _destination,
-            _hashLock
-        ));
-        require(transferId == _transferId,"[Brigde] Transfer ID don't match");
-        require(InboundTxMapping[_transferId].exists == false, "[Bridge] Trasfer Id already exists");
+            false,
+            false,
+            false,
+            true
+        );
+        bytes32 transferId = keccak256(
+            abi.encodePacked(_sender, _amount, _destination, _hashLock)
+        );
+        require(transferId == _transferId, "[Brigde] Transfer ID don't match");
+        require(
+            InboundTxMapping[_transferId].exists == false,
+            "[Bridge] Trasfer Id already exists"
+        );
         InboundTxMapping[_transferId] = inboundTxToBridge;
         emit NewTransferAvailable(_sender);
-        debt +=_amount;
+        debt += _amount;
         withdrawableMapping[_sender] = _amount;
-     }
+    }
 
-    function pause() public virtual onlyRole(DEFAULT_ADMIN_ROLE){
+    function pause() public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
         _pause();
     }
 
-    function unpause() public virtual onlyRole(DEFAULT_ADMIN_ROLE){
-        
+    function unpause() public virtual onlyRole(DEFAULT_ADMIN_ROLE) {
         _unpause();
     }
 
-    function setDestinationAddress(Network _network, address _bridgeAddress) public  onlyRole(DEFAULT_ADMIN_ROLE){
+    function setDestinationAddress(
+        Network _network,
+        address _bridgeAddress
+    ) public onlyRole(DEFAULT_ADMIN_ROLE) {
         bridgesAddresses[_network] = _bridgeAddress;
     }
 
     function withdraw(bytes32 _transferId) public nonReentrant {
         //checks
-        require(withdrawableMapping[msg.sender]>0,"[Bridge] No funds to widthdraw"); 
-        require(InboundTxMapping[_transferId].exists == true,"[Bridge] Transfer ID doesn't exists");
-        require(block.timestamp>InboundTxMapping[_transferId].timeLock,"[Bridge] Timelock didn't expired");
-        require(InboundTxMapping[_transferId].withdrawn == false, "[Bridge] Transfer ID was already widthdrawn");
-        require(InboundTxMapping[_transferId].refunded == false, "[Bridge] Transfer ID was refunded");
-        require(InboundTxMapping[_transferId].isDone == false,"[Bridge] Transfer ID was already commited");
+        require(
+            withdrawableMapping[msg.sender] > 0,
+            "[Bridge] No funds to widthdraw"
+        );
+        require(
+            InboundTxMapping[_transferId].exists == true,
+            "[Bridge] Transfer ID doesn't exists"
+        );
+        require(
+            block.timestamp > InboundTxMapping[_transferId].timeLock,
+            "[Bridge] Timelock didn't expired"
+        );
+        require(
+            InboundTxMapping[_transferId].withdrawn == false,
+            "[Bridge] Transfer ID was already widthdrawn"
+        );
+        require(
+            InboundTxMapping[_transferId].refunded == false,
+            "[Bridge] Transfer ID was refunded"
+        );
+        require(
+            InboundTxMapping[_transferId].isDone == false,
+            "[Bridge] Transfer ID was already commited"
+        );
         //Effects
-        uint amounToTx = withdrawableMapping[msg.sender]; 
+        uint amounToTx = withdrawableMapping[msg.sender];
         withdrawableMapping[msg.sender] = 0;
         debt -= amounToTx;
         //Interactions
-        emit Widthdraw(msg.sender,amounToTx);
-        IERC20(_LMT).transfer(msg.sender,amounToTx); 
-   
-
+        emit Widthdraw(msg.sender, amounToTx);
+        IERC20(_LMT).transfer(msg.sender, amounToTx);
     }
-
 }
